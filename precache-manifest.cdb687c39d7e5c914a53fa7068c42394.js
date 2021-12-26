11433395caf6f9299d9b72b4a74a1b4255882dca
Juan Camilo <jcczurg@gmail.com>
2021-12-26 04:06:34 -0500

    Updates

--- precache-manifest.cdb687c39d7e5c914a53fa7068c42394.js
+++ precache-manifest.cdb687c39d7e5c914a53fa7068c42394.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ffd28976b1d5d19aa4b4e823c8bd958",
+    "revision": "91d182a0998b096ffaa6ad4e42ef6c54",
     "url": "/ar-episode1-starter/index.html"
   },
   {
-    "revision": "ec8cbf13221d9aa6f324",
-    "url": "/ar-episode1-starter/static/css/main.8ae64874.chunk.css"
+    "revision": "241615c25a96d31abc7d",
+    "url": "/ar-episode1-starter/static/css/main.f496a4bf.chunk.css"
   },
   {
     "revision": "82dfc8ffdda3c0584c38",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ar-episode1-starter/static/js/2.467f60bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ec8cbf13221d9aa6f324",
-    "url": "/ar-episode1-starter/static/js/main.369af4aa.chunk.js"
+    "revision": "241615c25a96d31abc7d",
+    "url": "/ar-episode1-starter/static/js/main.46f35058.chunk.js"
   },
   {
     "revision": "d1ced229b758219096ea",
